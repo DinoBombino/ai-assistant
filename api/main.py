dc4d68f934a5ed0749544833d4f0212c1b94d792
dmitriy <korepanov.dmitriy.04@mail.ru>
2025-09-28 16:04:44 +0800

    For deploy in vercel_v4

--- api/main.py
+++ api/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173", "https://ai-assistant-4mbu.vercel.app"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class ChatRequest(BaseModel):
     message: str
